3c5ece6321c6a0bdfada8ae50c0d8f295e768889
Grzegorz Cagara <grzegorzcagara@gmail.com>
2025-02-23 21:28:42 +0100

    *hide menu for medium screen too

--- src/components/menu/NavMenu.styled.tsx
+++ src/components/menu/NavMenu.styled.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const LinkSpan = styled.span`
     margin-left: 1rem;
 
-    @media (max-width: 767px) {
+    @media (max-width: 991px) {
         display: none;
     }
 `
