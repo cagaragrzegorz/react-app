611de96087adee429f1db63e1959036252f1f9ad
Grzegorz Cagara <grzegorzcagara@gmail.com>
2025-11-28 19:30:36 +0100

    *refactoring and supplementing home view

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {Container} from "react-bootstrap";
+import {PageTitle, PageTitleDescription} from "../../components/common/Common.styled";
+import styled from "styled-components";
 
 export const Home: React.FC = () => (
     <Container fluid>
-        <Row>
-            <Col>
-                <h2>Home Page</h2>
-                <p>This page is meant to contain miscellaneous projects/subpages that are part of one website build with React
-                    and Bootstrap.
-                </p>
-                <p>I've build plenty different solutions along my career.
-                    Now I want to build portfolio of different use cases for testers and IT projects.
-                </p>
-            </Col>
-        </Row>
+        <PageTitle>Home Page</PageTitle>
+        <PageTitleDescription>This page is meant to contain miscellaneous projects that are part of one website build
+            with React and Bootstrap.</PageTitleDescription>
+        <StyledP>
+            Currently, it includes:
+            <ul>
+                <li>Todo: A simple todo list application to manage tasks.</li>
+                <li>Generators: Tool for generating various content for library.</li>
+                <li>Test Status Dashboard: A dashboard for monitoring E2E/API test runs.</li>
+            </ul>
+            More features and projects will be added in the future.
+        </StyledP>
+        <StyledP>
+            Check out my personal website at <a href="https://cagaragrzegorz.github.io/" target="_blank" rel="noopener noreferrer">https://cagaragrzegorz.github.io/</a>
+        </StyledP>
     </Container>
 );
+
+export const StyledP = styled.p`
+    font-weight: normal;
+`
